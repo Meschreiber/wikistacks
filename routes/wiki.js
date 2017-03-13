9020210508cce2fe3117765a2b0c0052bac9e634
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-03-13 14:20:10 -0400

    can see urlTitle in body

--- routes/wiki.js
+++ routes/wiki.js
@@ -15,11 +15,13 @@ router.post('/', function(req, res, next) {
 
 	var page = Page.build({
 	    title: req.body.title,
-		content: req.body.pagecontent
+		content: req.body.pagecontent,
+		status: req.body.status
 	  });
 
 	page.save();
-	res.redirect('/');
+	// res.redirect('/');
+	res.json(page);
 
     //res.json(req.body)
     //res.send('got to POST /wiki/');
@@ -27,6 +29,20 @@ router.post('/', function(req, res, next) {
 
 router.get('/add', function(req, res, next) {
     res.render('addpage');
+});
+
+router.get('/:urlTitle', function(req, res, next) {
+    // res.send(req.params.urlTitle);
+	  Page.findOne({ 
+	    where: { 
+	      urlTitle: req.params.urlTitle 
+	    } 
+	  })
+	  .then(function(foundPage){
+	    res.json(foundPage);
+	  })
+	  .catch(next);
+
 });
 
 module.exports = router;
